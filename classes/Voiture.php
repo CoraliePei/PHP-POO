7f3861d579d3f54c75a2a9becf98b4ff848062d6
Coralie Pei <pei.coralie@gmail.com>
2022-07-21 11:52:17 +0200

    Méthodes set et get pour les propriétés private

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -2,14 +2,47 @@
 
 // Attention : le typage est du PHP8
 
-class Voiture {
-    // Propriétés
-    public string $marque;
-    public string $couleur;
+// Encapsulation : public, protected, private
+
+class Voiture
+{
+    // **** Propriétés ****
+    private string $marque;
+    private string $couleur;
     public int $nbrRoues = 4; // Valeur par défaut
 
-    // Méthodes
-    public function demarrer() {
+    // **** Méthodes ****
+
+    // Récupérer la marque de la voiture private
+    public function getMarque(): string
+    {
+        return $this->marque;
+    }
+
+    // Définir la marque de la voiture private
+    public function setMarque($marque): void
+    {
+        $this->marque = $marque;
+    }
+
+    public function getColor(): string
+    {
+        return $this->couleur;
+    }
+
+    // Définir la marque de la voiture private
+    public function setColor($color): void
+    {
+        $this->couleur = $color;
+    }
+
+    public function demarrer()
+    {
         echo "<p>La voiture démarre</p>";
+    }
+
+    public function afficherCouleur(): string
+    {
+        return "<p>La voiture est de couleur $this->couleur </p>";
     }
 }
--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 require_once './classes/Voiture.php';
 
 $voiture1 = new Voiture();
-$voiture1->couleur = "rouge";
-$voiture1->marque = "BMW";
-$voiture1->couleur = "vert";
+// $voiture1->couleur = "rouge";
+// $voiture1->couleur = "vert";
+// $voiture1->marque = "BMW";
+$voiture1->setMarque("BMW");
+echo $voiture1->getMarque();
+echo "<br>";
+$voiture1->setColor("pourpre");
+echo $voiture1->getColor();
+
+
 
 $voiture2 = new Voiture();
-$voiture2->couleur = "jaune";
-$voiture2->marque = "Mercedes";
+// $voiture2->couleur = "jaune";
+// $voiture2->marque = "Mercedes";
 $voiture2->demarrer();
 
+// Pour une fonction return et public
+$couleurVoiture2 = $voiture2->afficherCouleur();
+echo $couleurVoiture2;
 
 echo "<pre>";
 var_dump($voiture1);
